e7850ba4072c6ce9784c18257c7b5a0e72087ab3
nuyr <other@nuyr.ru>
2026-04-24 18:49:23 +0300

    2.1 Web Workers API - Задание 1

---
+++ module-2/thread.js
@@ -0,0 +1,16 @@
+const slowFunction = (timeout = 3000) => {
+    let start = performance.now();
+    let x = 0;
+    let i = 0;
+
+    do {
+        i += 1;
+        x += (Math.random() - 0.5) * i;
+    } while (performance.now() - start < timeout);
+
+    return x;
+}
+
+const result = slowFunction();
+
+postMessage(result);
